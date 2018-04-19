e24ac2f87e7100b4f3701c4a21546a08857a7295
Nschulz88 <natalie.schulz@me.com>
2018-04-19 20:42:47 +0000

    Server for all functions incl. individual user colors

--- server.js
+++ server.js
@@ -17,18 +17,23 @@ const server = express()
 // Create the WebSockets server
 const wss = new WebSocket.Server({ server });
 
+let webSocketsInUse = {};
+
+var colors = [ 'red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange' ];
+colors.sort(function(a,b) { return Math.random() > 0.5; } );
+
 // This is a function defined to broadcast all messages to all "online" users
 wss.broadcast = function broadcast(data) {
-        console.log("logging JSON.parse(data)", JSON.parse(data));
-        let checkingWhatTypeOfMessage = JSON.parse(data);
-        // console.log("----------wss.clients.size", wss.clients.size);
+    console.log("logging JSON.parse(data)", JSON.parse(data));
+    let checkingWhatTypeOfMessage = JSON.parse(data);
+    // console.log("----------wss.clients.size", wss.clients.size);
 
-            wss.clients.forEach(function each(client) {
-                // console.log("logging client", client.readyState, WebSocket.OPEN );
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(data);
-            }
-            });
+    wss.clients.forEach(function each(client) {
+        // console.log("logging client", client.readyState, WebSocket.OPEN );
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(data);
+        }
+    });
 };
 
 const showActiveUsersToAll = () => {
@@ -38,31 +43,45 @@ const showActiveUsersToAll = () => {
     }));
 };
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
-  console.log('Client connected');
-  console.log("----------wss.clients.size", wss.clients.size);
-  showActiveUsersToAll();
-  
-  ws.on('message', (message) => {
-    let parsedMessage = JSON.parse(message);
-    const outgoingMessage = {
-        id: uuidv4(),
-        username: parsedMessage.username || 'Anonymous',
-        content: parsedMessage.content,
-        type: (parsedMessage.type === 'postMessage') ? 'incomingMessage' : 'incomingNotification',
-        activeUsers: wss.clients.size
-    };
-    wss.broadcast(JSON.stringify(outgoingMessage));
-  });
+    console.log('Client connected');
+    // console.log('log ws: ', ws);
+    // let userID = req.headers['sec-websocket-key'];
+    // console.log('Logging user ID', userID);
+    ws.color = getRandomColor();
+    console.log("----------wss.clients.size", wss.clients.size);
+    showActiveUsersToAll();
 
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => {
-  console.log('Client disconnected');
-  showActiveUsersToAll();
-  }
-);
+  
+    ws.on('message', (message) => {
+        let parsedMessage = JSON.parse(message);
+        const outgoingMessage = {
+            id: uuidv4(),
+            username: parsedMessage.username || 'Anonymous',
+            content: parsedMessage.content,
+            type: (parsedMessage.type === 'postMessage') ? 'incomingMessage' : 'incomingNotification',
+            activeUsers: wss.clients.size,
+            userColor:  ws.color 
+        };
+        wss.broadcast(JSON.stringify(outgoingMessage));
+    });
+
+    // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+    ws.on('close', () => {
+        console.log('Client disconnected');
+        showActiveUsersToAll();
+    });
 
 });
